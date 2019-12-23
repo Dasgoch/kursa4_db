328b9ee97bb63281856cb5c317ed5bd91de8053f
Dasgoch <34867943+Dasgoch@users.noreply.github.com>
2019-12-24 01:18:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import view
 import psycopg2
 
 con = psycopg2.connect(
-    database="postgres",
-    user="postgres",
-    password="1234",
-    host="127.0.0.1",
+    database="d80d6ankv1spjo",
+    user="zqpibjvpnhqcvv",
+    password="03db86573ea167252b57198f00a688d34f6869c5012ecd6865208a57086d3cc0",
+    host="ec2-54-247-177-254.eu-west-1.compute.amazonaws.com",
     port="5432"
 )
 cur = con.cursor()
